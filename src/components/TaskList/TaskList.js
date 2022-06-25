8aaba3330f838f59cf7faf55442c57b1ad0f108a
Mohammad Ghasemi <mohstarsos1@Mohammads-MacBook-Pro.local>
2022-06-25 16:48:55 +0430

    add components

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Task from '../Task/Task'
+
+const TaskList = () => {
+  return (
+    <div>
+        <Task />
+        <Task />
+        <Task />
+    </div>
+  )
+}
+
+export default TaskList
