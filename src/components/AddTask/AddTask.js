71c5eb1c17ff68d6f66043a52538226209242bfe
Mohammad Ghasemi <mohstarsos1@Mohammads-MacBook-Pro.local>
2022-06-27 17:44:47 +0430

    add some

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 const AddTask = () => {
+  const [data, setData] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/tasks", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ title: data, completed: false }),
+    });
+    const responseData = response.json();
+    console.log(responseData);
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
-        <button>Add Task</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+        />
+        <button type="submit">Add Task</button>
       </form>
     </div>
   );
